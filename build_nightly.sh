b6a38ebe771eb248ac9ff8b366ac43945082574b
reigadegr <272268642@qq.com>
2025-08-09 20:40:34 +0800

    perf: 优化编译参数

--- build_nightly.sh
+++ build_nightly.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 export RUSTFLAGS="
+    -C default-linker-libraries \
+    -Z external-clangrt \
+    -Z macro-backtrace \
     -Z remap-cwd-prefix=. \
     -Z dep-info-omit-d-target \
     -Z merge-functions=aliases \
     -C llvm-args=-enable-ml-inliner=release \
+    -C llvm-args=-regalloc-enable-advisor=release \
+    -C llvm-args=-hot-cold-split=true \
+    -C llvm-args=-enable-scalable-autovec-in-streaming-mode \
     -C llvm-args=-inliner-interactive-include-default \
     -C llvm-args=-ml-inliner-model-selector=arm64-mixed \
     -C llvm-args=-ml-inliner-skip-policy=if-caller-not-cold \

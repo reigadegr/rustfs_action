ab8b858d4335fa143f8eb3f936013a3b5516cbcd
reigadegr <272268642@qq.com>
2025-08-10 16:54:27 +0800

    perf: 优化编译参数，提升性能

--- build_nightly.sh
+++ build_nightly.sh
@@ -6,20 +6,22 @@ export RUSTFLAGS="
     -Z remap-cwd-prefix=. \
     -Z dep-info-omit-d-target \
     -Z merge-functions=aliases \
+    -C llvm-args=-aggressive-ext-opt \
     -C llvm-args=-enable-ml-inliner=release \
     -C llvm-args=-regalloc-enable-advisor=release \
     -C llvm-args=-hot-cold-split=true \
+    -C llvm-args=-enable-misched \
     -C llvm-args=-enable-scalable-autovec-in-streaming-mode \
     -C llvm-args=-inliner-interactive-include-default \
     -C llvm-args=-ml-inliner-model-selector=arm64-mixed \
     -C llvm-args=-ml-inliner-skip-policy=if-caller-not-cold \
-    -C link-args=-fomit-frame-pointer \
     -C llvm-args=-mergefunc-use-aliases \
     -C llvm-args=-enable-shrink-wrap=1 \
     -C llvm-args=-enable-gvn-hoist \
     -C llvm-args=-enable-loop-versioning-licm \
+    -C link-args=-fomit-frame-pointer \
     -C link-args=-Wl,-O3,--gc-sections,--as-needed \
-    -C link-args=-Wl,-z,norelro,-x,-s,--strip-all,-z,now
+    -C link-args=-Wl,-z,norelro,-x,-s,--strip-all,-z,--relax
 " 
 
 rm -rf Cargo.lock

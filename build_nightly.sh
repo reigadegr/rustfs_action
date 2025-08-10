a886aa090fdb7689c44de31dd97922e191ae2886
reigadegr <272268642@qq.com>
2025-08-10 13:14:23 +0800

    fix: 路径错误

--- build_nightly.sh
+++ build_nightly.sh
@@ -28,4 +28,4 @@ export CARGO_TERM_COLOR=always
 
 cargo +nightly zigbuild -r --target "$1" -p rustfs --bins -Z build-std -Z trim-paths
 
-dd if="$(dirname "$0")"/target/*-unknown-linux-musl/release/rustfs of="$(dirname "$0")"/module/rustfs
+dd if=./target/"$1"/release/rustfs of=./module/rustfs

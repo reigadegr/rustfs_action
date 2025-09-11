e3054e36de501f2d913fe261fb81e43195837be5
reigadegr <2722688642@qq.com>
2025-09-11 19:53:56 +0800

    refactor: 优化windows脚本

---
+++ x86_64-pc-windows-msvc_module/init_rustfs_env.bat
@@ -0,0 +1,10 @@
+setx RUSTFS_ACCESS_KEY "rfsaccess1"
+setx RUSTFS_SECRET_KEY "rfssecret"
+
+setx RUSTFS_VOLUMES ".\data_rfs"
+setx RUSTFS_ADDRESS ":9190"
+setx RUST_LOG warn
+
+setx RUSTFS_OBS_LOG_DIRECTORY ".\logs"  
+setx RUSTFS_SINKS_FILE_PATH ".\logs"
+

553a9fdba06941f24079329a39512f2967aa0b00
reigadegr <2722688642@qq.com>
2025-09-27 09:12:14 +0800

    chore: 删除不必要的转义

--- build_nightly_windows.sh
+++ build_nightly_windows.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 export RUSTFLAGS="
-    -Z mir-opt-level=2 \
-    -Z dylib-lto=yes \
-    -Z inline-mir=yes \
-    -Z fewer-names=yes \
-    -Z box-noalias=yes \
-    -Z share-generics=yes \
-    -Z remap-cwd-prefix=. \
-    -Z mutable-noalias=yes \
-    -Z function-sections=yes \
-    -Z dep-info-omit-d-target \
-    -Z flatten-format-args=yes \
-    -Z saturating-float-casts=yes \
-    -Z mir-enable-passes=+Inline \
-    -Z precise-enum-drop-elaboration=yes \
-    -C relro-level=full \
-    -C code-model=small \
-    -C default-linker-libraries \
-    -C target-feature=+crt-static \
+    -Z mir-opt-level=2
+    -Z dylib-lto=yes
+    -Z inline-mir=yes
+    -Z fewer-names=yes
+    -Z box-noalias=yes
+    -Z share-generics=yes
+    -Z remap-cwd-prefix=.
+    -Z mutable-noalias=yes
+    -Z function-sections=yes
+    -Z dep-info-omit-d-target
+    -Z flatten-format-args=yes
+    -Z saturating-float-casts=yes
+    -Z mir-enable-passes=+Inline
+    -Z precise-enum-drop-elaboration=yes
+    -C relro-level=full
+    -C code-model=small
+    -C default-linker-libraries
+    -C target-feature=+crt-static
     -C symbol-mangling-version=v0
 " 
 
--- build_stable_linux.sh
+++ build_stable_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export RUSTFLAGS="
-    -C relro-level=full \
-    -C code-model=small \
-    -C relocation-model=static \
+    -C relro-level=full
+    -C code-model=small
+    -C relocation-model=static
     -C symbol-mangling-version=v0
 "
 

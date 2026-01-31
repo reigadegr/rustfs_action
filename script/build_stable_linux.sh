d3f2fb30b8b43f2558866d82ca14a3a6ecbe1d15
reigadegr <2722688642@qq.com>
2026-02-01 01:31:31 +0800

    同步最新方案

--- script/build_stable_linux.sh
+++ script/build_stable_linux.sh
@@ -3,8 +3,17 @@
 export RUSTFLAGS="
     -C relro-level=full
     -C code-model=small
-    -C relocation-model=static
+    -C default-linker-libraries
+    -C relocation-model=pic
     -C symbol-mangling-version=v0
+    -C link-arg=-fuse-ld=mold
+    -C llvm-args=-fp-contract=off
+    -C llvm-args=-enable-misched
+    -C llvm-args=-enable-post-misched
+    -C llvm-args=-enable-dfa-jump-thread
+    -C link-args=-Wl,--sort-section=alignment
+    -C link-args=-Wl,-O3,--gc-sections,--as-needed
+    -C link-args=-Wl,-x,-z,noexecstack,-s,--strip-all,--relax
 "
 
 
@@ -12,4 +21,4 @@ export CARGO_TERM_COLOR=always
 
 export JEMALLOC_SYS_DISABLE_WARN_ERROR=1
 
-cargo +stable zigbuild -r --target "$1" --bin "$2"
+cargo +stable build -r --target "$1" --bin "$2"
